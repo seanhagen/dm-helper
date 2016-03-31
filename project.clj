ed8a91b26c91e4124a05b82e8cbc241687df91bb
Sean Patrick Hagen <shagen@bardel.ca>
2016-03-30 18:07:02 -0700

    MORE STUFF

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.commons/commons-io "1.3.2"]
+                 [seesaw "1.4.5"]]
   :main ^:skip-aot dm-helper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/dm_helper/core.clj
+++ src/dm_helper/core.clj
@@ -1,157 +1,66 @@
 (ns dm-helper.core
   (:gen-class)
-  (:require [clojure.java.io :as io]
-            [clojure.xml :as xml]
-            [clojure.zip :as zip]
-            [clojure.string :as string]))
-
-(defn zip-str [s]
-  (zip/xml-zip
-   (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
-
-(defn data-file [file]
-  (let [res (io/resource file)]
-    (if (nil? res)
-      (io/file file)
-      (io/file res))))
-
-(defn get-name
-  "WHEEE"
-  [thing]
-  (-> thing :content first :content first))
-
-;; (defn get-stat
-;;   "Fetch stat"
-;;   [thing stat]
-
-;;   (let [attr (filter (fn [x] (= (:tag x) stat)) (:content thing))]
-;;     (-> attr first :content first))
-;;   ;; (println "Got thing: " thing)
-;;   ;; (println "Got stat: " stat)
-;;   ;; 1
-;;   )
-
-;; (defn stats
-;;   "Show basic stats"
-;;   [thing]
-;;   (let [name (get-name thing)
-;;         strength (get-stat thing :str)
-;;         dex (get-stat thing :dex)
-;;         con (get-stat thing :con)
-;;         intel (get-stat thing :int)
-;;         wis (get-stat thing :wis)
-;;         cha (get-stat thing :cha)
-;;         cr (get-stat thing :cr)]
-;;     (printf "Name: %s (%s) STR: %s DEX: %s CON: %s INT: %s WIS: %s CHA: %s\n" name cr strength dex con intel wis cha)
-;;     ))
-
-(defn stat-to-map
-  "Turns {:tag :name, :content [Ancient Gold Dragon]} into {:name 'Ancient Gold Dragon'}"
-  [stat]
-  {(stat :tag) (first (stat :content))})
-
-(def f "test.xml")
-(def d (:content (first (zip-str (slurp (data-file f))))))
-(def bits (group-by
-           :tag
-           (:content
-            (second d)
-            )
-           ))
-
-(defn monster
-  [m]
-  (group-by :tag (:content m)))
-
-(defn nth-monster
-  [i]
-  (monster (nth d i)))
-
-(defn fix-tag
-  [action]
-  (let [content (:content action)
-        name (first (:content (first content)))
-        text (first (:content (second content)))]
-    {:name name :text text}))
-
-(defn needs-better-name
-  ""
-  [thing]
-  (let [name (first thing)
-        tags (second thing)]
-    (if (= (count tags) 1)
-      (stat-to-map (first tags))
-      {name (into [] (map fix-tag tags))}
-      )))
-
-(defn filter-creature
-  "Find a creature in the list"
-  [monsters looking-for]
-
-  (println "Trying to find: " looking-for)
-
-  (let [pattern (re-pattern looking-for)]
-
-    (into []
-          (filter
-           #(not
-             (nil?
-              (do
-                (println "Does '" (:name %) "' match? => " (not (nil? (re-find pattern (:name % )))))
-                (re-find pattern (:name %)))))
-           monsters))
-    )
-  )
+  ;; [org.apache.commons.io.FilenameUtils :as FilenameUtils]
+  (:import [org.apache.commons.io.FilenameUtils])
+  (:require [dm-helper.files.monster :as file-monster]
+            [dm-helper.files.spell :as file-spell]
+            [dm-helper.files.utils :as futil]
+            [seesaw.core :as sc]))
 
 (defn -main
   "LEEEARNING"
   [& args]
-  (println "Starting...")
 
-  (let [arg-file (first args)
-        file (if (nil? arg-file) "test.xml" arg-file)
-        data (:content (first (zip-str (slurp (data-file file)))))
-        monsters (map #(map needs-better-name (monster %)) data)]
+  (let [monsters (file-monster/load-all-monsters (first args))
+        m (file-monster/load-monsters-from-file (first monsters))]
 
-    ;; (println (group-by :tag (:content (last data))))
-    ;; (println (transform-stats (:content (last data))))
-
-    (def fixed-monsters
-      (loop [list data
-             result []]
-        (let [d (first list)
-              r (rest list)
-              m (into {} (map needs-better-name (monster d)))]
-          (conj result m)
-          (if (> (count r) 1)
-            (recur r result)
-            result)
-          )))
-
-    (println "Fixed monsters: " fixed-monsters)
-
-    (loop [list fixed-monsters]
-      (let [m (first list)
-            r (rest list)]
-        (println "Monster: " (:name m))
-        (if (> (count r) 1)
-          (recur r))
-        )
-      )
-
-    ;; (println "Filtered for 'Ancient':\n" (filter-creature fixed-monsters "Ancient"))
-
-    ;; (loop [list (filter-creature fixed-monsters "Ancient")]
-    ;;   (let [m (first list)
-    ;;         r (rest list)]
-
-    ;;     (println "Thing: " (:name m))
-
-    ;;     (if (> (count r) 1)
-    ;;       (recur r)))
+    ;; (loop [files monsters]
+    ;;   (let [])
     ;;   )
 
-    ;; (println (first monsters))
+    (println "monsters: " (first monsters))
 
-    ;;(dorun (map stats data))
-    ))
+    (println "what: " (count m))
+    )
+
+  ;; (let [files (futil/monster-files (first args))]
+  ;;   ;;(println "extensions?" (map .getExtension files) "\n\n")
+  ;;   (println "files: " (map #(FilenameUtils/getExtension %) (map  #(-> (.getPath %)) files)))
+  ;;   )
+
+
+  ;; (let [arg-file (first args)
+  ;;       spells (file-spell/load-spells-from-file arg-file)]
+  ;;   (printf "first spell" (first spells))
+  ;;   )
+
+  ;; (sc/native!)
+
+  ;; (println "Starting...")
+
+  ;; (sc/invoke-later
+  ;;  (-> (sc/frame :title "Hello"
+  ;;                :content "Hello Seesaw!"
+  ;;                :on-close :exit)
+  ;;      sc/pack!
+  ;;      sc/show!))
+
+  ;; (time
+  ;;  (let [arg-file (first args)
+  ;;        monsters (file-monster/load-monsters-from-file arg-file)]
+
+  ;;    (loop [list (file-monster/filter-creature monsters "Dragon")]
+  ;;      (let [m (first list)
+  ;;            r (rest list)]
+  ;;        (println "Creature: " (:name m))
+  ;;        (if (> (count r) 0)
+  ;;          (recur r))
+  ;;        )
+  ;;      )
+  ;;    )))
+
+  )
+
+(def f "test.xml")
+(def d (:content (first (futil/zip-str (slurp (futil/data-file f))))))
+(def bits (group-by :tag (:content (second d))))
